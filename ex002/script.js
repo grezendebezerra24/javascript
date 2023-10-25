b060d53fd75385a427e02cab724a2ae23e45f565
Rezende <grezendebezerra24@gmail.com>
2023-10-24 23:35:38 -0300

    corrigindo exercicio

--- ex002/script.js
+++ ex002/script.js
@@ -2,8 +2,3 @@
 var nome = window.prompt('Qual é seu nome ?')
 window.alert('E um grande prazer te conhecer, ' + nome + " !")
 
-var n1= window.prompt('Digite um número:  ')
-var n2 = window.prompt('Digite Outro numero:  ')
-
-var soma = n1 + n2
-window.alert('A soma dos valores é;  ' + soma)
